23279337289ed99542735357593f9bf7ca5a584e
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-11 02:36:25 +0600

    Code Refactoring

--- firebase-auth/src/contexts/AuthContext.js
+++ firebase-auth/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
-import { auth } from "./../firebase";
+import React, {useContext, useEffect, useState} from "react";
+import {auth} from "../firebase";
 
 const AuthContext = React.createContext();
 
@@ -14,10 +14,9 @@ const AuthProvider = (props) => {
   };
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    return auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
     });
-    return unsubscribe;
   }, []);
 
   return (
