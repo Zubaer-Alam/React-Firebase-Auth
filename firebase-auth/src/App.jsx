1c13d54144ff0adf9b83f6c01633fb3c27b25bc1
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-11 15:19:59 +0600

    Ported to Vite

---
+++ firebase-auth/src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `${count}`;
+    }, [count]);
+
+    return (
+        <div>
+            <p>Count:{count}</p>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+        </div>
+    )
+}
