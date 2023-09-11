64dd93ae666f87a3163727580530eb1dc04e63a5
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-11 16:35:43 +0600

    Install firebase

--- firebase-auth/src/App.jsx
+++ firebase-auth/src/App.jsx
@@ -1,17 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
-function App() {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        document.title = `${count}`;
-    }, [count]);
-
-    return (
+function App(){
+    return(
         <div>
-            <p>Count:{count}</p>
-            <button onClick={() => setCount(count + 1)}>Increase</button>
-            <button onClick={() => setCount(count - 1)}>Decrease</button>
+            My App
         </div>
     )
 }
